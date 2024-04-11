9faf2cc1ea7fb0975640d0d13894f62a6333621c
klaoun <36087587+klaoun@users.noreply.github.com>
2024-04-11 02:54:31 +0200

    clean up SMTP.php

--- include/phpmailer/SMTP.php
+++ include/phpmailer/SMTP.php
@@ -268,7 +268,7 @@ class SMTP
         }
         //Is this a PSR-3 logger?
         if ($this->Debugoutput instanceof \Psr\Log\LoggerInterface) {
-            $this->Debugoutput->debug($str);
+        $this->Debugoutput->debug(rtrim($str, "\r\n"));
 
             return;
         }
